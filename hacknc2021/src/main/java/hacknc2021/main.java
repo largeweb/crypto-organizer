780a239bc45cb8e6d3d8aca28f4ff482d0c480d3
matt <mat@unc.edu>
2021-11-06 17:18:08 -0400

    sending from script

--- hacknc2021/src/main/java/hacknc2021/main.java
+++ hacknc2021/src/main/java/hacknc2021/main.java
@@ -14,17 +14,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//vars.setup();
-		//startGUI = new StartGUI();
-
-
-		API apiTest = new API("binance");
-
-		List<AssetBalance> list = apiTest.getBinanceAccountBalance();
-
-		for(int i = 0; i < list.size(); i++){
-			System.out.println(apiTest.getBinanceAccountBalance().get(i));
-		}
+		vars.setup();
+		startGUI = new StartGUI();
 
 
 	}
