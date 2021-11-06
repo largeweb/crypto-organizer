1993245ce54641ef4494593cc6357aa39b6ebe76
matt <mat@unc.edu>
2021-11-06 18:24:53 -0400

    sending from script

--- hacknc2021/src/main/java/hacknc2021/main.java
+++ hacknc2021/src/main/java/hacknc2021/main.java
@@ -14,8 +14,18 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		vars.setup();
-		startGUI = new StartGUI();
+//		vars.setup();
+//		startGUI = new StartGUI();
+		JavaSoundRecorder recorder = new JavaSoundRecorder(); 
+		recorder.record();
+		
+//	      Define an audio format of the sound source to be captured, using the class AudioFormat.
+//          Create a DataLine.Info object to hold information of a data line.
+//          Obtain a TargetDataLine object which represents an input data line from which audio data can be captured, using the method getLineInfo(DataLine.Info) of the AudioSystem class.
+//          Open and start the target data line to begin capturing audio data.
+//          Create an AudioInputStream object to read data from the target data line.
+//          Record the captured sound into a WAV file using the following method of the class AudioSystem:
+
 	}
 
 }
