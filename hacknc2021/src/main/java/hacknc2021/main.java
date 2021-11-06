db33fe37b9d8693fda3c6ad879c36fcda358d8fc
helenmcginnis <helennameliaa@gmail.com>
2021-11-06 19:12:13 -0400

    gfhf

--- hacknc2021/src/main/java/hacknc2021/main.java
+++ hacknc2021/src/main/java/hacknc2021/main.java
@@ -9,6 +9,7 @@ import io.api.etherscan.model.Price;
 
 public class main {
 	// hello
+	
 	public static StartGUI startGUI;
 	public static DashboardGUI dashboardGUI;
 
@@ -16,29 +17,29 @@ public class main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// vars.setup();
-		// startGUI = new StartGUI();
+		vars.setup();
+		startGUI = new StartGUI();
 
 
-		final JavaSoundRecorder recorder = new JavaSoundRecorder();
- 
-        // creates a new thread that waits for a specified
-        // of time before stopping
-        Thread stopper = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(RECORD_TIME);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-                recorder.finish();
-            }
-        });
- 
-        stopper.start();
- 
-        // start recording
-        recorder.start();
+//		final JavaSoundRecorder recorder = new JavaSoundRecorder();
+// 
+//        // creates a new thread that waits for a specified
+//        // of time before stopping
+//        Thread stopper = new Thread(new Runnable() {
+//            public void run() {
+//                try {
+//                    Thread.sleep(RECORD_TIME);
+//                } catch (InterruptedException ex) {
+//                    ex.printStackTrace();
+//                }
+//                recorder.finish();
+//            }
+//        });
+// 
+//        stopper.start();
+// 
+//        // start recording
+//        recorder.start();
 	}
 
 }
